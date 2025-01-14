a2636c94d4c86cf235b17be6cb7f63463a6cd7fa
Chew Yi Siang <chewyisiang123@gmail.com>
2025-01-14 21:18:32 +0800

    update on timeline

--- templates/static/index.js
+++ templates/static/index.js
@@ -26,11 +26,3 @@ animations.animations();
         lastScroll = currentScroll
 
     })
-
-// timeline js below
-document.querySelectorAll('.timeline-node').forEach(node => {
-    node.addEventListener('click', function() {
-        const content = this.querySelector('.node-content');
-        content.classList.toggle('expanded');
-    });
-});
